37d0fa95635c39c303e42641c7628eb4128d75ab
abdul <abdul.tanveer@gmail.com>
2021-06-26 14:09:50 +0530

    hardcoded data --mocked ratings call and mocked movie info call

--- MovieCatalog-dnc/src/main/java/dnc/sp/ms/mcatalog/resources/MovieCatalogResource.java
+++ MovieCatalog-dnc/src/main/java/dnc/sp/ms/mcatalog/resources/MovieCatalogResource.java
@@ -19,7 +19,7 @@ public class MovieCatalogResource {
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		//1. get all the rated movie ids freom the ratings service
-		List<Rating> ratings = Arrays.asList(  //mocking call to ratings ms
+		List<Rating> ratingsBag = Arrays.asList(  //mocking call to ratings ms
 				new Rating("movieOne",1),
 				new Rating("movieTwo",2),
 				new Rating("movieThree",3)
@@ -27,9 +27,9 @@ public class MovieCatalogResource {
 		
 		
 		//for each movie id retrieved from the previous MS call send it to MovieInfo and get movie details
-		return ratings.stream()   //putting the list of items [emptying the bag on a] conveyor belt
-				.map(rating -> new CatalogItem("movieOne", "description", 1))   //converting each rating item moving on the conveyor belt TO  catalogitem
-				.collect(Collectors.toList());  //after all the items have been converted put it back into the bag[list]
+		return ratingsBag.stream()                                                  //putting the list of items [emptying the bag on a] conveyor belt
+				.map(rating -> new CatalogItem(rating.getMovieId(), "description of "+ rating.getMovieId(), rating.getRating()))   //converting each rating item moving on the conveyor belt TO  catalogitem
+				.collect(Collectors.toList());                                                                                     //after all the items have been converted put it back into the bag[list]
 		
 		//put them together and serve the user
 		
